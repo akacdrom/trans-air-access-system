327bcfff0436ae498be69f978405542d4622239c
Ahmet <ahmetakkusx@gmail.com>
2022-05-30 14:59:29 +0200

    tsconfig

--- dist/card/ConcreteCard.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.ConcreteCard = void 0;
-var ConcreteCard = /** @class */ (function () {
-    function ConcreteCard() {
-    }
-    return ConcreteCard;
-}());
-exports.ConcreteCard = ConcreteCard;
